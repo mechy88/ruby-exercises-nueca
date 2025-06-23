d0e10f7a93bd96accc0515e1b4f30051d15977c8
mechy88 <miguelandrerepatacodo@gmail.com>
2025-06-23 15:32:08 +0800

    Randomized

--- ruby/challenge/simulations.rb
+++ ruby/challenge/simulations.rb
@@ -37,10 +37,6 @@ end
 
 def queue_a(serving, tellers)
   serving.each do |customer|
-    a = tellers.tell_a.length
-    b = tellers.tell_b.length
-    c = tellers.tell_c.length
-
     fall_in_line_random(customer, tellers)
   end
 end
@@ -65,70 +61,15 @@ def fall_in_line_efficient(customer, tellers, a_queue, b_queue, c_queue)
   end
 end
 
-# def push_to_random(queueus)
-  
-# end
-
 def fall_in_line_random(customer, tellers)
-  min = [tellers.tell_a, tellers.tell_b, tellers.tell_c].min_by(&:length)
-  mins = queueus.select { |queue| queue.length == min }
-  mins.sample.push()
+  lines = [
+    { line: tellers.tell_a, multiplier: tellers.a },
+    { line: tellers.tell_b, multiplier: tellers.b },
+    { line: tellers.tell_c, multiplier: tellers.c }
+  ]
+  min = lines.min_by { |line| line[:line].length }
+  min = min[:line].length
+  mins = lines.select { |queue| queue[:line].length == min }
+  chosen = mins.sample
+  chosen[:line].push(customer * chosen[:multiplier])
 end
-
-# # Returns the average time senario A takes over
-# def run_simulation_a(customers)
-#   dispatcher = Dispatcher.new(customers)
-#   tellers = LocalProcessingSystem.new
-#   minutes = 0.0
-
-#   until tellers.finished? && dispatcher.finished?
-#     minutes += 1
-#     serving = dispatcher.dispatch
-
-#     serving.each do |customer|
-#       a = tellers.tell_a.length
-#       b = tellers.tell_b.length
-#       c = tellers.tell_c.length
-
-#       if c < b && c < a
-#         tellers.push_c(customer * tellers.c)
-#       elsif b < a
-#         tellers.push_b(customer * tellers.b)
-#       else
-#         tellers.push_a(customer * tellers.a)
-#       end
-#     end
-
-#     tellers.process
-#   end
-#   minutes
-# end
-
-# # Returns the average time senario B takes over
-# def run_simulation_b(customers)
-#   dispatcher = Dispatcher.new(customers)
-#   tellers = LocalProcessingSystem.new
-#   minutes = 0.0
-
-#   until tellers.finished? && dispatcher.finished?
-#     minutes += 1
-#     serving = dispatcher.dispatch
-
-#     serving.each do |customer|
-#       a = dispatcher.queue_compute(tellers.tell_a)
-#       b = dispatcher.queue_compute(tellers.tell_b)
-#       c = dispatcher.queue_compute(tellers.tell_c)
-
-#       if c < b && c < a
-#         tellers.push_c(customer * tellers.c)
-#       elsif b < a
-#         tellers.push_b(customer * tellers.b)
-#       else
-#         tellers.push_a(customer * tellers.a)
-#       end
-#     end
-
-#     tellers.process
-#   end
-#   minutes
-# end
